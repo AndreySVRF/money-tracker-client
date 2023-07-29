49ac6e4e6c12322b30f215605f0e69ee34ed3948
AndreySVRF <andreysmagin14@gmail.com>
2023-07-29 15:50:16 +0400

    Add icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as LightModeIcon } from './light-mode-icon.svg';
+export { ReactComponent as DarkModeIcon } from './dark-mode-icon.svg';
+export { ReactComponent as SystemModeIcon } from './system-mode-icon.svg';
