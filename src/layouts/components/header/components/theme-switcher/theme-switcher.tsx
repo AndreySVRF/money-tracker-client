2bd9d84c9639f48ac4f24c1451112b4bf5a21838
AndreySVRF <andreysmagin14@gmail.com>
2023-07-29 16:02:15 +0400

    Add text to ThemeSwitcher if not hand over icons

--- src/layouts/components/header/components/theme-switcher/theme-switcher.tsx
+++ src/layouts/components/header/components/theme-switcher/theme-switcher.tsx
@@ -1,16 +1,7 @@
 import { useTheme } from './hooks';
-import {
-  DarkModeIcon,
-  LightModeIcon,
-  SystemModeIcon
-} from '../../../../../assets';
 
 const ThemeSwitcher = () => {
-  const { themeOptions, setTheme } = useTheme({
-    light: <LightModeIcon className="fill-amber-500 dark:fill-amber-300" />,
-    dark: <DarkModeIcon className="fill-blue-300 dark:fill-amber-100" />,
-    system: <SystemModeIcon className="fill-gray-600 dark:fill-blue-100" />
-  });
+  const { themeOptions, setTheme } = useTheme();
 
   const themeOptionsRender = themeOptions.map((themeOption) => (
     <button
@@ -18,8 +9,10 @@ const ThemeSwitcher = () => {
       onClick={() => setTheme(themeOption.text)}
       key={themeOption.text}
     >
-      {!!themeOption.icon && (
+      {themeOption.icon ? (
         <span className="flex h-5 w-5">{themeOption.icon}</span>
+      ) : (
+        <span className="capitalize">{themeOption.text}</span>
       )}
     </button>
   ));
