519e792aa15fffe16327585d756ffa6816678f13
AndreySVRF <andreysmagin14@gmail.com>
2023-08-04 19:06:36 +0400

    Add api

---
+++ src/api/axios.api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { getFromLocalStorage, TOKEN_KEY } from '../utils';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer + ${getFromLocalStorage(TOKEN_KEY)}` || ''
+  }
+});
+
+export { instance };
