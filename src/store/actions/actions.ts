5a62d3581d5107daeb15a82ae2c3b61269c28938
AndreySVRF <andreysmagin14@gmail.com>
2023-09-08 05:37:16 +0400

    Update store

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -1,9 +1,14 @@
-import { resetCategoriesState, resetUserState } from '../../modules';
+import {
+  resetCategoriesState,
+  resetTransactionsState,
+  resetUserState
+} from '../../modules';
 import { Dispatch } from '@reduxjs/toolkit';
 
 const resetStore = (dispatch: Dispatch): void => {
   dispatch(resetUserState());
   dispatch(resetCategoriesState());
+  dispatch(resetTransactionsState());
 };
 
 export { resetStore };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from '../modules/auth';
 import categoriesReducer from '../modules/categories';
+import transactionsReducer from '../modules/transactions';
 
 export const rootReducer = combineReducers({
   user: userReducer,
-  categories: categoriesReducer
+  categories: categoriesReducer,
+  transactions: transactionsReducer
 });
 
 export const store = configureStore({
